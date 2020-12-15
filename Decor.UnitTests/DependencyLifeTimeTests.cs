a0c0903190b36f69ebe61426e2ee34094c2392b7
Laurynas Lazauskas <laurynas@lazauskas.info>
2020-12-15 18:06:54 +0200

    Add dependency life time test

---
+++ Decor.UnitTests/DependencyLifeTimeTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Decor.UnitTests
+{
+    public class DependencyLifeTimeTests
+    {
+        [Fact]
+        public void I_can_decorate_with_decorator_that_has_scoped_dependency()
+        {
+            // Arrange
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<Dependency>()
+                .AddTransient<TestDecoratorWithDependency>()
+                .AddScoped<ISomeInterface, SomeClass>()
+                .Decorate<ISomeInterface>()
+                .BuildServiceProvider();
+
+            // Act & Assert
+            serviceProvider.GetRequiredService<ISomeInterface>();
+        }
+
+        #region Setup
+        public class Dependency { }
+
+        public class TestDecoratorWithDependency : IDecorator
+        {
+            public TestDecoratorWithDependency(Dependency dependency)
+            {
+                Dependency = dependency;
+            }
+
+            public Dependency Dependency { get; }
+
+            public Task OnInvoke(Call call) => Task.CompletedTask;
+        }
+
+        public interface ISomeInterface
+        {
+            void SomeMethod();
+        }
+
+        public class SomeClass : ISomeInterface
+        {
+            [Decorate(typeof(TestDecoratorWithDependency))]
+            public void SomeMethod() { }
+        }
+        #endregion
+    }
+}
